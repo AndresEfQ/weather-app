6231aa7e41709e6e9595a4d7e7c49886eb69bb51
AndresEfQ <andresf2641@gmail.com>
2022-10-29 11:16:46 -0500

    v-1.0.0

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const cityInput = document.querySelector('input');
 const apiKey = 'b80b52c331368860cdc190459543327e';
 let units = 'C';
+
+const unitsButton = document.getElementById('units_button')
+
 let city = cityInput.value || 'Medellin';
 
 const weatherName = document.getElementById('weather_name');
@@ -34,7 +37,7 @@ const icons = {
   '13n': 'snow.svg',
   '50d': 'mist.svg',
   '50n': 'mist.svg'
-}
+};
 
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
@@ -51,19 +54,19 @@ const capitalize = (string) => {
 }
 
 const getWeather = () => {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`, {mode: 'cors'})
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units === 'C' ? 'metric' : 'imperial'}`, {mode: 'cors'})
   .then(function (response) {
     return response.json();
   })
   .then(function (response) {
     const now = new Date();
     weatherName.textContent = capitalize(response.weather[0].description);
-    cityName.textContent = response.name;
+    cityName.textContent = `${response.name}. ${response.sys.country}`;
     date.textContent = `${weekDays[now.getDay()]}, ${now.getDate()}th ${months[now.getMonth()]} ${now.getFullYear()}`;
     time.textContent = `${now.getHours()}:${now.getMinutes()}`;
     temperature.textContent = `${response.main.temp.toFixed(1)} °${units}`;
     /* mainIcon.src = `./assets/SVG/${icons['01n']}`; */
-    mainIcon.source = `./assets/SVG/${icons[response.weather[0].icon]}`;
+    mainIcon.src = `./assets/SVG/${icons[response.weather[0].icon]}`;
     console.log(response);
   })
   .catch(function (err) {
@@ -72,16 +75,58 @@ const getWeather = () => {
 }
 
 const getForecast = () => {
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`, {mode: 'cors'})
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units === 'C' ? 'metric' : 'imperial'}`, {mode: 'cors'})
   .then(function (response) {
     return response.json();
   })
   .then(function (response) {
-    
+    for (let i = 0; i < dayNames.length; i++) {
+      const index = 6 + (i * 8);
+      const forecastDate = response.list[index].dt_txt;
+      const dayNumb = new Date(forecastDate).getDay();
+      const day = weekDays[dayNumb];
+      const temp = response.list[index].main.temp.toFixed(1);
+      const minTemp = response.list[index].main.temp_min.toFixed(1);
+      const iconCode = response.list[index].weather[0].icon;
+      const iconSrc = `./assets/SVG/${icons[iconCode]}`;
+      console.log(iconSrc);
+      
+      dayNames[i].textContent = day;
+      dayTemp[i].textContent = `${temp} °${units}`;
+      dayMinTemp[i].textContent = `${minTemp} °${units}`;
+      dayIcons[i].src = iconSrc;
+    }
+    console.log(response);
   })
   .catch(function (err) {
     throw new Error(err);
   })
 }
-getWeather();
-getForecast();
+
+getWeather()
+getForecast()
+
+const changeUnits = () => {
+  if (units === 'C') {
+    units = 'F';
+  } else if (units === 'F') {
+    units = 'C';
+  }
+  unitsButton.textContent = `Temp in °${units === 'C' ? 'F' : 'C'}`;
+  getWeather();
+  getForecast();
+}
+
+const changeCity = (e) => {
+  console.log(e)
+  console.log(cityInput.value)
+  if (e.key === 'Enter') {
+    city = cityInput.value;
+    cityInput.value = '';
+    getWeather();
+    getForecast();
+  }
+}
+
+unitsButton.addEventListener('click', changeUnits);
+cityInput.addEventListener('keydown', changeCity);
